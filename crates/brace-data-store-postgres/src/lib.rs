9ce5874fda1c971b7cb52611bc02b6b12dba2706
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-15 15:10:59 +0100

    Add postgres connection pooling (#36)

--- crates/brace-data-store-postgres/src/lib.rs
+++ crates/brace-data-store-postgres/src/lib.rs
@@ -1,29 +1,29 @@
-use futures::future::FutureExt;
-use tokio::spawn;
-use tokio_postgres::{Client, Config, Error, NoTls};
+use bb8::{Pool, PooledConnection, RunError};
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::{Config, Error, NoTls};
 
 use brace_util_future::result::FutureResult;
 
-pub struct Postgres(Config);
+pub type FutureConnection<'a> =
+    FutureResult<'a, PooledConnection<'a, PostgresConnectionManager<NoTls>>, RunError<Error>>;
+
+pub struct Postgres(Pool<PostgresConnectionManager<NoTls>>);
 
 impl Postgres {
-    pub fn new(config: Config) -> Self {
-        Self(config)
+    pub fn new<'a>(config: Config) -> FutureResult<'a, Self, Error> {
+        FutureResult::from_future(async {
+            let manager = PostgresConnectionManager::new(config, NoTls);
+            let pool = Pool::builder().build(manager).await?;
+
+            Ok(Self(pool))
+        })
     }
 
-    pub fn config(&self) -> &Config {
+    pub fn pool(&self) -> &Pool<PostgresConnectionManager<NoTls>> {
         &self.0
     }
 
-    pub fn connect(&self) -> FutureResult<Client, Error> {
-        FutureResult::from_future(async move {
-            self.config()
-                .connect(NoTls)
-                .await
-                .map(|(client, connection)| {
-                    spawn(connection.map(|_| ()));
-                    client
-                })
-        })
+    pub fn connect(&self) -> FutureConnection {
+        FutureResult::from_future(async move { self.pool().get().await })
     }
 }
--- crates/brace-data-store-postgres/tests/integration.rs
+++ crates/brace-data-store-postgres/tests/integration.rs
@@ -1,13 +1,14 @@
 use std::str::FromStr;
 
+use bb8::RunError;
 use tokio_postgres::{Config, Error};
 
 use brace_data_store_postgres::Postgres;
 
 #[tokio::test]
-async fn test_postgres_connect() -> Result<(), Error> {
+async fn test_postgres_connect() -> Result<(), RunError<Error>> {
     let config = Config::from_str("postgresql://postgres:postgres@localhost:5432")?;
-    let postgres = Postgres::new(config);
+    let postgres = Postgres::new(config).await?;
     let conn = postgres.connect().await?;
     let row = conn.query_one("SELECT 42", &[]).await?;
     let res = row.get::<usize, i32>(0);
